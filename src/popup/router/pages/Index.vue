637278a0584b065ced30b9b8658a79ffa379a505
cheng.zhang <mr.cheng94@foxmail.com>
2020-03-16 19:06:34 +0800

    change css process to less

--- src/popup/router/pages/Index.vue
+++ src/popup/router/pages/Index.vue
@@ -1,5 +1,37 @@
 <template>
-  <p>Hello world!</p>
+  <div class='container'>
+        <form>
+            <div class="grouppanel">
+                <div class="grouptitle">
+                    <span class="title" data-i18n="baTitle">Options</span>
+                    <span class="more" id="more">...</span>
+                </div>
+                <div class="crosspanel">
+                    <label data-i18n="baEnabled">Enabled</label>
+                    <div class="enabled">
+                        <input type="checkbox" value=true id="enabled">
+                        <label for="enabled"></label>
+                    </div>
+    
+                    <label data-i18n="baHotKey">AuoSel.</label>
+                    <select id="hotkey">
+                        <option value="0">Off</option>
+                        <option value="16">Shift</option>
+                        <option value="17">Ctrl</option>
+                        <option value="18">Alt</option>
+                    </select>
+                    <hr class="anki-options">
+                    <select id="deckname" class="anki-options col-2x">
+                        <option value=""></option>
+                    </select>
+                    <hr>
+                    <select id="dict" class="col-2x">
+                        <option value="encn-Default">encn-Default</option>
+                    </select>
+                </div>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
@@ -10,8 +42,6 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-p {
-  font-size: 20px;
-}
+<style lang="less" scoped>
+    
 </style>
